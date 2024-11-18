ecdfd35f9b55e6a75df9555186606808a45f2560
Maggie <maggli@indeed.com>
2024-11-18 17:31:30 -0500

    adding revenue file

---
+++ detailed_rev.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 16:48:24 2024
+
+@author: maggli
+"""
+
+
+
+def sales_rev(sales_id: tuple) -> str:
+    """
+    pull the sales revenue based on a given list of sales id
+
+    Parameters
+    ----------
+    sales_id : tuple
+        DESCRIPTION.
+
+    Returns
+    -------
+    str
+        DESCRIPTION.
+
+    """
+    pass
+
+def country_rev(country: tuple) -> str:
+    """
+    pull the country level revenue based on a given list of country
+
+    Parameters
+    ----------
+    country : tuple
+        DESCRIPTION.
+
+    Returns
+    -------
+    str
+        DESCRIPTION.
+
+    """
+    pass
+
+
+
