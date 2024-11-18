c135ed6a187fbed05cf936f4795eae42b6a1f31f
Maggie <maggli@indeed.com>
2024-11-18 17:31:44 -0500

    adding all files

---
+++ revenue_analysis.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 16:44:53 2024
+
+@author: maggli
+"""
+
+def read_file():
+    
+    pass
+
+
+def covert_currency():
+    pass
+
+
+def get_team():
+    
+    pass
+
+
+def get_currency_code():
+    pass
+
+
+def get_quarterly_rev():
+    pass
+
+
+def get_team_rev():
+    pass
+
+def create_new_table():
+    pass
+
+
+def main():
+        
+    pass
+
+
+if __name__== '__main__':
+    pass
